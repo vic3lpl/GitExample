bacc7669bacfcb35a6a82b9893d128faa6275936
IRS <IRS@rd03mac.local>
2017-08-16 13:18:35 +0800

    again

---
+++ GitExample/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  GitExample
+//
+//  Created by IRS on 15/08/2017.
+//  Copyright © 2017 IrsSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+- (IBAction)clickSecondBtn:(id)sender;
+
+@end
---
+++ GitExample/SecondViewController.m
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.m
+//  GitExample
+//
+//  Created by IRS on 15/08/2017.
+//  Copyright © 2017 IrsSoftware. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"ffffffff");
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)clickSecondBtn:(id)sender {
+    NSLog(@"gggggggg");
+}
+@end
